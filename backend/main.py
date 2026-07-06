360b73cc1a428f677b23f67b6fec6b04a94a1179
Gaal Jaylaani <gaaljaylaani@Gaals-MacBook-Air.local>
2026-07-06 11:39:07 -0700

    Finished issue 33

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,38 @@
 from fastapi import FastAPI
-from backend.api.routes import router as jpdb_routes
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+from pydantic import BaseModel
+
+load_dotenv()
 
 app = FastAPI()
-app.include_router(jpdb_routes.router)
+supabase: Client = create_client(os.environ.get("SUPABASE_URL"), os.environ.get("SUPABASE_KEY"))
+
+
+class StatsCreate(BaseModel):
+    known_words: int
+    number_messages: int
+    steak: bool
+
+
+@app.get('/user_languages')
+async def get_user_languages():
+    response = supabase.table('user_languagges').select('*').execute()
+    return response.data
+
+@app.get('/known_words_user_statistics')
+async def get_known_words_user_statistics():
+    response = supabase.table('temporal_user_statistics').select('known_words').execute()
+    return response.data
+
+@app.get('/user_statistics')
+async def get_user_statistics():
+    response = supabase.table('temporal_user_statistics').select('*').execute()
+    return response.data
+
+
+@app.post('/user_statistics', status_code=201)
+async def post_user_statistics(stat: StatsCreate):
+    response = supabase.table('temporal_user_statistics').insert(stat.model_dump()).execute()
+    return response.data
