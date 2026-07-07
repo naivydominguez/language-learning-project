080e776dfb8a6f7ef83b1e82b9b121b79bb9b3bc
Ethan Cabalona <cabalona.e@northeastern.edu>
2026-07-07 12:36:37 -0400

    removed typo that blocked tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-t /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
