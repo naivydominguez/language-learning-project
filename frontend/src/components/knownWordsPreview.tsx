feaf72350b4f8ed0792986d0bd3920b3e81b582f
NKGuillaume <gnanfack@terpmail.umd.edu>
2026-07-08 20:43:14 -0400

    done

---
+++ frontend/src/components/knownWordsPreview.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const KnownWordsPreview = () => {
+  return (
+    <View className="w-full h-max flex flex-col bg-white p-4 rounded-md border border-background-dark">
+      <Text className="font-bold text-lg">Known Words</Text>
+      {/* calendar grid */}
+    </View>
+  );
+};
+
+export default KnownWordsPreview;
